c72e1f866bf2d1dd6a72cdb762b6f0e49f5dc34f
dvl666 <kamagaspar@gmail.com>
2023-11-10 13:34:09 -0300

    Primer commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:cinemapedia/config/router/app_router.dart';
+import 'package:cinemapedia/config/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      theme: AppTheme().getTheme(),
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
