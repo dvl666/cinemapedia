71d31f8ea9b2741389f59aa649769ecac7260976
dvl666 <kamagaspar@gmail.com>
2023-11-21 18:34:16 -0300

    feat(lib/infrastructure/mappers): add movieDetailsToEntity

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
@@ -21,5 +22,26 @@ class MovieMapper {
     video: movieDB.video,
     voteAverage: movieDB.voteAverage,
     voteCount: movieDB.voteCount
+  );
+
+  static Movie movieDetailsToEntity(MovieDetails movie) => Movie(
+    adult: movie.adult, 
+    backdropPath: (movie.backdropPath != '')
+      ? 'https://image.tmdb.org/t/p/w500${movie.posterPath}'
+      : 'no-poster',
+    genreIds: movie.genres.map((e) => e.name).toList(), 
+    id: movie.id, 
+    originalLanguage: movie.originalLanguage, 
+    originalTitle: movie.originalTitle, 
+    overview: movie.overview, 
+    popularity: movie.popularity, 
+    posterPath:(movie.posterPath != '')
+      ? 'https://image.tmdb.org/t/p/w500${movie.posterPath}'
+      : 'no-poster', 
+    releaseDate: movie.releaseDate, 
+    title: movie.title, 
+    video: movie.video, 
+    voteAverage: movie.voteAverage, 
+    voteCount: movie.voteCount
   );
 }
