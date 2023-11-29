cf42a5dca645d90371e65ac90be60ad3ef060ad7
dvl666 <kamagaspar@gmail.com>
2023-11-29 15:13:31 -0300

    bottom navigation bar update

--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
   const CustomBottomNavigation({super.key});
 
+  void onItemTapped(BuildContext context, int index){
+    switch(index){
+      case 0:
+        context.go('/');
+      case 1: 
+        context.go('/');
+      case 2:
+        context.go('/favorites');
+    }
+  }
+
+  int getCurrentIndex(BuildContext context){
+    
+    final String location = GoRouterState.of(context).uri.toString();
+
+    switch(location){
+      case '/':
+        return 0;
+      case '/categories':
+        return 1;
+      case '/favorites':
+        return 2;
+      default:
+        return 0;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+
+
+
     return BottomNavigationBar(
       elevation: 0,
+      currentIndex: getCurrentIndex(context),
+      onTap: (value){
+        onItemTapped(context, value);
+      },
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.home_max_outlined),
@@ -17,7 +52,7 @@ class CustomBottomNavigation extends StatelessWidget {
           label: 'Categorias'
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.favorite_outlined), 
+          icon: Icon(Icons.favorite_outline_outlined), 
           label: 'Favoritos'
         ),
       ],
